51bddd9cf23b5e19404c75b18f1c7a75156cf132
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2023-06-20 16:13:10 -0300

    Update configureATK.sh

--- configureATK.sh
+++ configureATK.sh
@@ -1,8 +1,7 @@
-sudo apt-get install git && 
-cd /mnt/c &&
+sudo apt-get install git -y && 
 mkdir php &&
 cd php &&
-sudo apt install php-cli &&
+sudo apt install php-cli -y &&
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php &&
 php composer-setup.php && 
 mv composer.phar composer &&
@@ -10,7 +9,7 @@ chmod +x composer &&
 sudo mv composer /usr/local/bin/composer && #Movendo composer de lugar para facilitar a execução
 git clone https://github.com/Sintattica/atk-skeleton.git conference && #Clonagem de projeto de exemplo
 cd conference && 
-sudo apt install docker.io &&
-sudo apt install docker-compose
+sudo apt install docker.io -y &&
+sudo apt install docker-compose -y
 
 echo "Finalizando Instalacao"
