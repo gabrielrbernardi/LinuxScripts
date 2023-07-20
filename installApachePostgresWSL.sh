aa4df306d6479a80c98bba774f2e1b04f50db297
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2023-07-20 10:41:24 -0300

    Update installApachePostgresWSL.sh

--- installApachePostgresWSL.sh
+++ installApachePostgresWSL.sh
@@ -1,5 +1,6 @@
 #installing dependencies
 sudo apt-get remove php* -y && 
+sudo apt-get update -y &&
 sudo add-apt-repository ppa:ondrej/php -y &&
 sudo apt install apache2 php7.4 php7.4-fpm -y && 
 sudo apt-get install php7.4-mysql php7.4-pdo php7.4-pgsql -y &&
@@ -10,7 +11,6 @@ sudo apt install libpq-dev -y &&
 sudo service apache2 start &&
 
 #changing directory to update files
-cd /etc/apache2 && 
 wget https://raw.githubusercontent.com/gabrielrbernardi/LinuxScripts/main/apache2.conf && 
 wget https://raw.githubusercontent.com/gabrielrbernardi/LinuxScripts/main/000-default.conf && 
 wget https://raw.githubusercontent.com/gabrielrbernardi/LinuxScripts/main/php.ini && 
@@ -18,7 +18,12 @@ mv apache2.conf /etc/apache2/ &&
 mv 000-default.conf /etc/apache2/sites-available/ && 
 mv php.ini /etc/php/7.4/fpm/ && 
 
+#cd /etc/apache2 && 
 #restarting services and preparing environment
 sudo service apache2 stop && sudo service apache2 start
 mv /mnt/c/projetoatk/{.,}* /var/www/html/ -R &&
+chmod 777 /var/www/html -R &&
+chown www-data:www-data /var/www/html -R &&
 sudo service apache2 stop && sudo service apache2 start
+
+echo "Finalizando Configuracao"
